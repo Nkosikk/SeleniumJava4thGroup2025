92a9143abca70146b1aa045ef2dc42d32418a4df
Nkosi <njceles@gmail.com>
2025-10-01 19:47:10 +0200

    enabled the run headless for edge

--- src/test/java/Utils/BrowserFactory.java
+++ src/test/java/Utils/BrowserFactory.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.Test;
@@ -20,7 +21,9 @@ public class BrowserFactory {
         } else if (browserChoice.equalsIgnoreCase("firefox")) {
             driver = new FirefoxDriver();
         } else if (browserChoice.equalsIgnoreCase("edge")) {
-            driver = new EdgeDriver();
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.addArguments("--headless");
+            driver = new EdgeDriver(edgeOptions);
         } else {
             driver = new SafariDriver();
         }
