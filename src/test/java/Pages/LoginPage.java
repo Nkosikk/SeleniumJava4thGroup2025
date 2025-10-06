ae3ec5bcfe7689525f320c4ba6d20596562d72ba
juddb <juddburger@gmail.com>
2025-10-06 16:38:28 +0200

    -Added file for first batch of assessment, Login & Session. -Edited spreadsheet to contain my test account. -Needs refactoring but running.

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,14 +1,10 @@
 package Pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-
 import java.time.Duration;
-
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 public class LoginPage {
