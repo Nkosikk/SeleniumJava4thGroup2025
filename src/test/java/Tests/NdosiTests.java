259a5a60612315c778097f55589b28fed7ba51a7
Nkosi <njceles@gmail.com>
2026-01-26 19:54:09 +0200

    enabled all the tests

--- src/test/java/Tests/NdosiTests.java
+++ src/test/java/Tests/NdosiTests.java
@@ -12,22 +12,22 @@ public class NdosiTests extends Base {
     }
 
 
-//    @Test(dependsOnMethods = "verifyHomePageIsDisplayedTests")
-//    public void clickLearningMaterialTests() {
-//        homePage.clickLearningMaterial();
-//    }
-//
-//    @Test(dependsOnMethods = "clickLearningMaterialTests")
-//    public void verifyLoginPageIsDisplayedTests() {
-//        loginPage.verifyLoginPageIsDisplayed();
-//    }
-//
-//    @Test(dependsOnMethods = "verifyLoginPageIsDisplayedTests")
-//    public void enterEmailAddressTests() {
-//        loginPage.enterEmailAddress(readFromExcel.username);
-//
-//    }
-//
+    @Test(dependsOnMethods = "verifyHomePageIsDisplayedTests")
+    public void clickLearningMaterialTests() {
+        homePage.clickLearningMaterial();
+    }
+
+    @Test(dependsOnMethods = "clickLearningMaterialTests")
+    public void verifyLoginPageIsDisplayedTests() {
+        loginPage.verifyLoginPageIsDisplayed();
+    }
+
+    @Test(dependsOnMethods = "verifyLoginPageIsDisplayedTests")
+    public void enterEmailAddressTests() {
+        loginPage.enterEmailAddress(readFromExcel.username);
+
+    }
+
     @AfterTest
     public void closeBrowser() {
         driver.quit();
