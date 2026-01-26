fdebd6cb2fca2f7941bf2b47e6157478a074081f
AvelaNoganta <avisnamsisi@gmail.com>
2026-01-26 19:26:10 +0200

    Commented out Tests

--- src/test/java/Tests/NdosiTests.java
+++ src/test/java/Tests/NdosiTests.java
@@ -11,22 +11,23 @@ public class NdosiTests extends Base {
         takesScreenshots.takesSnapShot(driver, "Home Page");
     }
 
-    @Test(dependsOnMethods = "verifyHomePageIsDisplayedTests")
-    public void clickLearningMaterialTests() {
-        homePage.clickLearningMaterial();
-    }
 
-    @Test(dependsOnMethods = "clickLearningMaterialTests")
-    public void verifyLoginPageIsDisplayedTests() {
-        loginPage.verifyLoginPageIsDisplayed();
-    }
-
-    @Test(dependsOnMethods = "verifyLoginPageIsDisplayedTests")
-    public void enterEmailAddressTests() {
-        loginPage.enterEmailAddress(readFromExcel.username);
-
-    }
-
+//    @Test(dependsOnMethods = "verifyHomePageIsDisplayedTests")
+//    public void clickLearningMaterialTests() {
+//        homePage.clickLearningMaterial();
+//    }
+//
+//    @Test(dependsOnMethods = "clickLearningMaterialTests")
+//    public void verifyLoginPageIsDisplayedTests() {
+//        loginPage.verifyLoginPageIsDisplayed();
+//    }
+//
+//    @Test(dependsOnMethods = "verifyLoginPageIsDisplayedTests")
+//    public void enterEmailAddressTests() {
+//        loginPage.enterEmailAddress(readFromExcel.username);
+//
+//    }
+//
     @AfterTest
     public void closeBrowser() {
         driver.quit();
